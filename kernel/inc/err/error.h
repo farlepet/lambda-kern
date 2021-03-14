8f91c16c86cf95870e0df6801652c87f2d76e1ad
Peter Farley <far.peter1@gmail.com>
2021-03-14 09:46:23 -0500

    error: Added kdebug function.

--- kernel/inc/err/error.h
+++ kernel/inc/err/error.h
@@ -14,10 +14,8 @@ typedef enum { //!< Enumeration of error levels
 /**
  * \brief Prints information about the kernel.
  * 
- * Disables interrupts so it will not get interrupted, checks to see if the 
- * error level is >= the minimum level, and if so, prints the current
- * clock tick, then prints the error message. Then, if en_int != 0, it
- * enables interrupts again.
+ * Checks to see if the  error level is >= the minimum level, and if so,
+ * prints the current clock tick, then prints the error message.
  * 
  * @param errlvl the severity of the message
  * @param msg the format string
@@ -25,22 +23,28 @@ typedef enum { //!< Enumeration of error levels
  */
 void kerror(error_level_e errlvl, char *msg, ...);
 
+/** Enumeration of debug message sources */
 typedef enum {
-
+	DEBUGSRC_FS      = 0, /** Kernel filesystem interface */
+	DEBUGSRC_MM      = 1, /** Memory management */
+	DEBUGSRC_PROC    = 2, /** Process management */
+	DEBUGSRC_EXEC    = 3, /** Process creation and execution */
+	DEBUGSRC_SYSCALL = 4, /** System calls */
+	DEBUGSRC_DRIVER  = 5  /** Driver loading/unloading and management */
 } debug_source_e;
 
 /**
  * \brief Prints debug info from a specified kernel source
  * 
- * Disables interrupts so it will not get interrupted, checks to see if the 
- * error level is >= the minimum level, and if so, prints the current
- * clock tick, then prints the error message. Then, if en_int != 0, it
- * enables interrupts again.
+ * Similar to kerror, except it checks if the corresponding bit is set in the
+ * debug mask rather than checking error level.
  * 
- * @param errlvl the severity of the message
+ * @param src the source of the message
  * @param msg the format string
  * @param ... the arguments to go along with the format string
+ * 
+ * @see kerror
  */
-void kdebug(debug_source_e errlvl, char *msg, ...)
+void kdebug(debug_source_e src, char *msg, ...);
 
 #endif
--- kernel/src/err/error.c
+++ kernel/src/err/error.c
@@ -6,20 +6,32 @@
 #include <types.h>
 #include <video.h>
 
-error_level minlvl = ERR_INFO; //!< Minimal level where messages are shown
+error_level_e minlvl    = ERR_INFO; //!< Minimal level where messages are shown
+uint32_t      debugmask = 0;
 
 lock_t kerror_lock = 0; //!< Only 1 message can be printed at a time
-/**
- * \brief Prints information about the kernel.
- * Checks to see if the error level is >= the
- * minimum level, and if so, prints the current
- * clock tick, then prints the error message.
- * @param errlvl the severity of the message
- * @param msg the format string
- * @param ... the arguments to go along with the format string
- */
-void kerror(error_level errlvl, char *msg, ...) {
+
+void kerror(error_level_e errlvl, char *msg, ...) {
 	if(errlvl >= minlvl) {
+		if(interrupts_enabled()) lock_for(&kerror_lock, 8); // We don't want something like a kernel message from a lost task stopping us
+
+		if(KERNEL_COLORCODE)
+			kprintf("\e[31m[\e[32m%X%08X\e[31m]\e[0m ", (uint32_t)(kerneltime >> 32), (uint32_t)kerneltime);
+		else
+			kprintf("[%X%08X] ", (uint32_t)(kerneltime >> 32), (uint32_t)kerneltime);
+
+		__builtin_va_list varg;
+		__builtin_va_start(varg, msg);
+		kprintv(msg, varg);
+		__builtin_va_end(varg);
+		kput('\n');
+		
+		if(interrupts_enabled()) unlock(&kerror_lock);
+	}
+}
+
+void kdebug(debug_source_e src, char *msg, ...) {
+	if(debugmask & (1UL << src)) {
 		if(interrupts_enabled()) lock_for(&kerror_lock, 8); // We don't want something like a kernel message from a lost task stopping us
 
 		if(KERNEL_COLORCODE)
