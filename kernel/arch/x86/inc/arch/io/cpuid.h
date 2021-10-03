78c764d8a8067aee43449c8c4c01688d746c042d
Peter Farley <far.peter1@gmail.com>
2021-10-03 10:51:35 -0500

    Add functions to access CPUID and MSRs.

---
+++ kernel/arch/x86/inc/arch/io/cpuid.h
@@ -0,0 +1,123 @@
+#ifndef ARCH_IO_CPUID_H
+#define ARCH_IO_CPUID_H
+
+#include <stdint.h>
+
+typedef enum {
+    CPUIDOP_MAXLEVELANDVENDOR = 0x00000000,
+    CPUIDOP_FEATURES          = 0x00000001,
+
+    CPUIDOP_SERIAL            = 0x00000003
+} cpuid_op_e;
+
+
+/**
+ * @brief Check if CPU supports CPUID instruction
+ * 
+ * @return 1 if supported, else 0
+ */
+static inline int cpuid_avail(void) {
+    /* TODO */
+    return 1;
+}
+
+/**
+ * @brief Read CPUID
+ * 
+ * @param op CPUID operation
+ * @param ret CPUID result, array of EAX, EBX, ECX, EDX
+ */
+static inline void cpuid(cpuid_op_e op, uint32_t ret[4]) {
+    asm volatile("cpuid" :
+                 "=a"(ret[0]),"=b"(ret[1]),"=c"(ret[2]),"=d"(ret[3]) :
+                 "a"(op));
+}
+
+#define CPUIDFEATURE_ECX_HV      (1UL << 31)
+#define CPUIDFEATURE_ECX_RDRAND  (1UL << 30)
+#define CPUIDFEATURE_ECX_F16C    (1UL << 29)
+#define CPUIDFEATURE_ECX_AVX     (1UL << 28)
+#define CPUIDFEATURE_ECX_OSXSAVE (1UL << 27)
+#define CPUIDFEATURE_ECX_XSAVE   (1UL << 26)
+#define CPUIDFEATURE_ECX_AES     (1UL << 25)
+#define CPUIDFEATURE_ECX_TSCD    (1UL << 24)
+#define CPUIDFEATURE_ECX_POPCNT  (1UL << 23)
+#define CPUIDFEATURE_ECX_MOVBE   (1UL << 22)
+#define CPUIDFEATURE_ECX_X2APIC  (1UL << 21)
+#define CPUIDFEATURE_ECX_SSE42   (1UL << 20)
+#define CPUIDFEATURE_ECX_SSE41   (1UL << 19)
+#define CPUIDFEATURE_ECX_DCA     (1UL << 18)
+#define CPUIDFEATURE_ECX_PCID    (1UL << 17)
+#define CPUIDFEATURE_ECX_PDCM    (1UL << 15)
+#define CPUIDFEATURE_ECX_ETPRD   (1UL << 14)
+#define CPUIDFEATURE_ECX_CX16    (1UL << 13)
+#define CPUIDFEATURE_ECX_FMA     (1UL << 12)
+#define CPUIDFEATURE_ECX_SDBG    (1UL << 11)
+#define CPUIDFEATURE_ECX_CID     (1UL << 10)
+#define CPUIDFEATURE_ECX_SSSE3   (1UL <<  9)
+#define CPUIDFEATURE_ECX_TM2     (1UL <<  8)
+#define CPUIDFEATURE_ECX_EST     (1UL <<  7)
+#define CPUIDFEATURE_ECX_SMX     (1UL <<  6)
+#define CPUIDFEATURE_ECX_VMX     (1UL <<  5)
+#define CPUIDFEATURE_ECX_DSCPL   (1UL <<  4)
+#define CPUIDFEATURE_ECX_MON     (1UL <<  3)
+#define CPUIDFEATURE_ECX_DTES64  (1UL <<  2)
+#define CPUIDFEATURE_ECX_PCLMUL  (1UL <<  1)
+#define CPUIDFEATURE_ECX_SSE3    (1UL <<  0)
+
+#define CPUIDFEATURE_EDX_PBE     (1UL << 31)
+#define CPUIDFEATURE_EDX_IA64    (1UL << 30)
+#define CPUIDFEATURE_EDX_TM1     (1UL << 29)
+#define CPUIDFEATURE_EDX_HTT     (1UL << 28)
+#define CPUIDFEATURE_EDX_SS      (1UL << 27)
+#define CPUIDFEATURE_EDX_SSE2    (1UL << 26)
+#define CPUIDFEATURE_EDX_SSE     (1UL << 25)
+#define CPUIDFEATURE_EDX_FXSR    (1UL << 24)
+#define CPUIDFEATURE_EDX_MMX     (1UL << 23)
+#define CPUIDFEATURE_EDX_ACPI    (1UL << 22)
+#define CPUIDFEATURE_EDX_DTES    (1UL << 21)
+#define CPUIDFEATURE_EDX_CLFL    (1UL << 19)
+#define CPUIDFEATURE_EDX_PSN     (1UL << 18)
+#define CPUIDFEATURE_EDX_PSE36   (1UL << 17)
+#define CPUIDFEATURE_EDX_PAT     (1UL << 16)
+#define CPUIDFEATURE_EDX_CMOV    (1UL << 15)
+#define CPUIDFEATURE_EDX_MCA     (1UL << 14)
+#define CPUIDFEATURE_EDX_PGE     (1UL << 13)
+#define CPUIDFEATURE_EDX_MTRR    (1UL << 12)
+#define CPUIDFEATURE_EDX_SEP     (1UL << 11)
+#define CPUIDFEATURE_EDX_APIC    (1UL <<  9)
+#define CPUIDFEATURE_EDX_CX8     (1UL <<  8)
+#define CPUIDFEATURE_EDX_MCE     (1UL <<  7)
+#define CPUIDFEATURE_EDX_PAE     (1UL <<  6)
+#define CPUIDFEATURE_EDX_MSR     (1UL <<  5)
+#define CPUIDFEATURE_EDX_TSC     (1UL <<  4)
+#define CPUIDFEATURE_EDX_PSE     (1UL <<  3)
+#define CPUIDFEATURE_EDX_DE      (1UL <<  2)
+#define CPUIDFEATURE_EDX_VME     (1UL <<  1)
+#define CPUIDFEATURE_EDX_FPU     (1UL <<  0)
+
+/**
+ * @brief Check if feature(s) reported in ECX register of CPUID 0x01 are present
+ * 
+ * @param features Feature(s) to check for
+ * @return 1 if present, else 0
+ */
+static inline int cpuid_featurecheck_ecx(uint32_t features) {
+    uint32_t ret[4];
+    cpuid(CPUIDOP_FEATURES, ret);
+    return ((ret[2] & features) == features);
+}
+
+/**
+ * @brief Check if feature(s) reported in EDX register of CPUID 0x01 are present
+ * 
+ * @param features Feature(s) to check for
+ * @return 1 if present, else 0
+ */
+static inline int cpuid_featurecheck_edx(uint32_t features) {
+    uint32_t ret[4];
+    cpuid(CPUIDOP_FEATURES, ret);
+    return ((ret[3] & features) == features);
+}
+
+#endif
---
+++ kernel/arch/x86/inc/arch/io/msr.h
@@ -0,0 +1,47 @@
+#ifndef ARCH_IO_MSR
+#define ARCH_IO_MSR
+
+#include <arch/io/cpuid.h>
+
+/**
+ * @brief Check if the CPU suports the MSR instructions
+ * 
+ * @return 1 if supported, else 0
+ */
+static inline int msr_avail(void) {
+    return cpuid_featurecheck_edx(CPUIDFEATURE_EDX_MSR);
+}
+
+typedef enum {
+    MSRREG_LOCALAPIC = 0x0000001B
+} msr_register_e;
+
+/**
+ * @brief Read current value of MSR
+ * 
+ * @param reg MSR to read
+ * @return value of MSR
+ */
+static inline uint64_t msr_read(msr_register_e reg) {
+    uint32_t eax, edx;
+    asm volatile("rdmsr" :
+                 "=a"(eax), "=d"(edx) :
+                 "c"(reg));
+    return ((uint64_t)edx << 32) | eax;
+}
+
+/**
+ * @brief Write value to MSR
+ * 
+ * @param reg MSR to write to
+ * @param value Value to write to MSR
+ */
+static inline void msr_write(msr_register_e reg, uint64_t value) {
+    uint32_t eax = (uint32_t)value;
+    uint32_t edx = (uint32_t)(value >> 32);
+    asm volatile("wrmsr" : :
+                 "a"(eax), "d"(edx), "c"(reg));
+}
+
+
+#endif
